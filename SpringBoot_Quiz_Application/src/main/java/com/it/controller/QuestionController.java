42998d42fe21df8f88cd0978c1a5fe592d075812
swapnilkature123 <swapnilkature917@gmail.com>
2025-06-06 23:26:29 +0530

    Quiz app

---
+++ SpringBoot_Quiz_Application/src/main/java/com/it/controller/QuestionController.java
@@ -0,0 +1,24 @@
+package com.it.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.it.Question;
+import com.it.service.QuestionService;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+	
+	@Autowired
+	QuestionService questionService;
+	
+	@GetMapping("allQuestions")
+	public List<Question> getAllQuestions() {
+		return questionService.getAllQuestions();
+	}
+}
